34433f62ac8782a7be4d588bc82511e1ab5cbcca
Md. Rayhanul Islam Raj <rayhanulraj210@gmail.com>
2023-05-16 10:41:36 +0600

    modified

--- Project1/test/Program.cs
+++ Project1/test/Program.cs
@@ -3,7 +3,7 @@ int t = int.Parse(Console.ReadLine());
 while (t-- > 0)
 {
     int n = int.Parse(Console.ReadLine());
-    for (int i = 0; i < n; i++)
-        Console.Write(n + " ");
+    for (int i = 1; i <= n; i++)
+        Console.Write(i*2 + " ");
     Console.WriteLine();
 }
