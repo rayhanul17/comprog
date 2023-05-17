fd81f4c0d489a0df050308bda5cb9e37fe92ae32
Md. Rayhanul Islam Raj <rayhanulraj210@gmail.com>
2023-05-17 22:04:01 +0600

    added

---
+++ Project1/M2/Program.cs
@@ -0,0 +1,43 @@
+﻿public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val=0, ListNode next=null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution
+{
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        string s1= string.Empty, s2 = string.Empty;
+        while(l1 != null || l2 != null) 
+        {
+            s1 = l1?.val+s1;
+            s2 = l2?.val+s2;
+            l1 = l1?.next;
+            l2 = l2?.next;
+        }
+        string n = (int.Parse(s1) + int.Parse(s2)).ToString();
+        ListNode listNode = new ListNode();
+        for(int i = n.Length-1; i >= 0; i--)
+        {
+
+        }
+
+        return null;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Solution s = new Solution();
+        ListNode l1 = new ListNode { val = 2, next = new ListNode { val = 4, next = new ListNode { val = 3 } } };
+        ListNode l2 = new ListNode { val = 5, next = new ListNode { val = 6, next = new ListNode { val = 4 } } };
+        s.AddTwoNumbers(l1, l2);
+    }
+}
---
+++ Project1/M24/Program.cs
@@ -0,0 +1,32 @@
+﻿public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0, ListNode next=null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution
+{    
+    public ListNode SwapPairs(ListNode head)
+    {
+        ListNode pointer = head;
+        while(pointer != null && pointer.next != null)
+        {
+            (pointer.val, pointer.next.val) = (pointer.next.val, pointer.val);
+            pointer = pointer.next.next;
+        }
+        return head;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Solution s = new Solution();
+        var ans = s.SwapPairs(new ListNode { val = 1, next = new ListNode { val = 2, next = new ListNode { val = 3, next = new ListNode { val = 4 } } } });
+        Console.WriteLine(ans);
+    }
+}
