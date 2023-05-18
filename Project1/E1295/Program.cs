51802a1bf02a1c5f75d273f090eccdf178498083
Md. Rayhanul Islam Raj <rayhanulraj210@gmail.com>
2023-05-18 23:39:33 +0600

    added

---
+++ Project1/E1295/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public int FindNumbers(int[] nums)
+    {
+        int count = 0;
+        foreach (int x in nums)        
+            if ((Math.Floor(Math.Log10(x)) + 1) % 2 == 0)
+                count++;
+        
+        return count;
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Solution s = new Solution();
+        var ans = s.FindNumbers(new int[] { 555, 901, 482, 1771 });
+        Console.WriteLine(ans);
+    }
+}
