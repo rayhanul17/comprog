c8bb9b0b2457259920bdce4677f178fbb7c13ae5
Rayhanul Islam Raj <rayhanulraj210@gmail.com>
2023-06-22 17:15:12 +0600

    add a codeforcesn partial solution

---
+++ Project1/Codeforces/Program.cs
@@ -0,0 +1,17 @@
+﻿//B1843
+int t = int.Parse(Console.ReadLine());
+
+while(t-- > 0)
+{
+    Console.ReadLine();
+    int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    long sum = Math.Abs(input[0]);
+    int c = 0;
+    for(int i = 1; i < input.Length; i++)
+    {
+        sum += Math.Abs(input[i]);
+        if ((input[i - 1] < 0 && input[i] > 0) || (input[i - 1] > 0 && input[i] < 0))
+            c++;
+    }
+    Console.WriteLine($"{sum} {c}");
+}
