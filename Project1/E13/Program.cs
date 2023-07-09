cb936f773618493b663510adb47f3a67ecc23d95
Rayhanul Islam Raj <rayhanulraj210@gmail.com>
2023-07-09 15:45:53 +0600

    Add more solutions

---
+++ Project1/E13/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Solution
+{
+    Dictionary<char, int> cv = new Dictionary<char, int>() { { 'I', 1 }, { 'V', 5 }, { 'X', 10 }, { 'L', 50 }, { 'C', 100 }, { 'D', 500 }, { 'M', 1000 } };
+    public int RomanToInt(string s)
+    {
+        if (s.Length == 1)
+            return cv[s[0]];
+        int v = 0;
+        for (int i = 0; i < s.Length - 1; i++)
+        {
+            if (cv[s[i]] >= cv[s[i + 1]])
+                v += cv[s[i]];
+            else
+                v += cv[s[i + 1]] - cv[s[i++]];
+        }
+        if (cv[s[s.Length-2]] >= cv[s[s.Length - 1]])
+            v += cv[s[s.Length-1]];
+        return v;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Solution s = new Solution();
+        var ans = s.RomanToInt(Console.ReadLine());
+        Console.WriteLine(ans);
+    }
+}
---
+++ Project1/E14/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        string ss = "";
+        int i = 0;
+        for(int j = 1; j < strs.Length; j++)
+        {
+            for(int k = 1; k < strs.Length; k++)
+            {
+                if(strs[0][i] != strs[k][j])
+                    break;
+            }
+            ss += strs[0][i];
+        }
+        return ss;
+    }
+}  
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Solution s = new Solution();
+        var ans = s.LongestCommonPrefix(new string[] {"dog", "racecar", "car"});
+        Console.WriteLine(ans);
+    }
+}
