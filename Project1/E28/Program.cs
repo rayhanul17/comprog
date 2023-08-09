13ad90ae6a2183eec9105653b02d70824afa7842
Rayhanul Islam Raj <rayhanulraj210@gmail.com>
2023-08-09 15:34:42 +0600

    leetcode e28

---
+++ Project1/E28/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public int StrStr(string haystack, string needle)
+    {
+        if (haystack.Contains(needle))            
+            return haystack.IndexOf(needle);        
+        else
+            return -1;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Solution s = new Solution();
+        var ans = s.StrStr("asadbutsad", "sad");
+        Console.WriteLine(ans);
+    }
+}
